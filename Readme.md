ff53f002db29d3f3786ecfb6ff225a46d8a6f51e
mmm25002500 <tershi@cutespirit.org>
2022-03-10 11:33:04 +0800

    Rewrite: Update to latest

--- Readme.md
+++ Readme.md
@@ -1,34 +1,34 @@
 ## Foodpanda爬蟲 Foodpanda-spider
-得到Foodpanda的所有地區,價格,或是餐點品名
+得到Foodpanda的所有地區、價格、或是餐點品名。
 
 ## 介紹 Introduction
-這是一款使用Python編成而成的Foodpanda爬蟲 內含地區選擇 餐點查看 以及價格查看 以便下單 不定期更新！
+這是一款使用Python編成而成的Foodpanda爬蟲，內含地區選擇，餐點查看，以及價格查看，以便下單，不定期更新！
+
 ## 安裝方式 Installation
+```sh
+sudo apt update
+sudo apt install python python2 python3
+sudo pip install bs4 requests
+git clone https://github.com/mmm25002500/foodpanda-spider/
+cd foodpanda-spider/
+python main.py
+```
 
-```sudo apt update``` <br>
-```sudo apt install python python2 python3```<br>
-```sudo pip install bs4 requests```<br>
-```git clone https://github.com/mmm25002500/foodpanda-spider/```<br>
-```cd foodpanda-spider/```<br>
-```python main.py```
 ## 用法 Usage
-
+```sh
 Usage: main.py [-h] [-v]
-
 -h, --help 顯示幫助頁面 show this help message and exit
-
--v ,--version 顯示腳本版本 show program's version number and exit
+-v, --version 顯示腳本版本 show program's version number and exit
+```
 
 ## 截圖 ScreenShot
-<img src = "img/Workspace 1_393.png" width ="600px"/>
-點擊看大圖<br>
+![logo](img/1.png)
 
 ## 關於我們 About Us
-
-[Team Website](www.tershi.ml)
-
-[Team Facebook](https://www.facebook.com/shanling.team/)
-
-[XiaTerShi YouTube](https://www.youtube.com/channel/UCPdpFDFOp3sPbZhRkaQVaQA)
-
-[XiaTerShi FaceBook](https://www.facebook.com/Tershi25648)
+[TershiXia's Personal Website](https://tershi.cutespirit.org)
+[TershiXia's Blog](https://blog.cutespirit.org/tershi)
+[TershiXia YouTube](https://www.youtube.com/channel/夏特稀)
+[Cutespirit Website](https://www.cutespirit.org)
+[Cutespirit-SHOP](https://shop.cutespirit.org)
+[Cutespirit Facebook](https://fb.cutespirit.org)
+[Cutespirit Discord](https://discord.cutespirit.org)
--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-# -*- coding: utf-8 -*-
-import bs4 ,requests ,time ,os ,argparse ,sys
+#  -*- coding: utf-8 -*-
+import bs4
+import requests
+import time
+import os
+import argparse
+import sys
 
-#幫助 python2 main.py --help
+# help python2 main.py --help
 parser=argparse.ArgumentParser(
-    description='''(C) Copyright Aishen-Shanling Team 2020(C) Tershi 2020
-		This is a Foodpanda-Spider which made by Xia-Tershi(夏特稀)
-		You can Use it for checking new store or menu that has "prices" on that 
+    description='''(C) Copyright Cutespirit Team 2022(C)
+		This is a Foodpanda-Spider which is made by TershiXia
+		You can use this application to check the new stores or menus and prices.
 		''',
-    epilog="""You can edit the file and sell the script to earn money. Share it to your all friends.
-		Open Source Python Script 開源Python腳本
+    epilog="""You can edit all files or commercial used. Share it with your friends.
+			Open Source Python Script.
 		""")
 parser.add_argument('-v','--version' ,action='version' , version = 'Tershi Foodpanda Version 1.5')
 args=parser.parse_args()
@@ -17,36 +22,36 @@ args=parser.parse_args()
 CityURL= 'https://www.foodpanda.com.tw'
 result = requests.get(CityURL)
 
-sp = bs4.BeautifulSoup(result.content , "html.parser")			#取的所有城市選單的HTML
-all_a = sp.find_all("a",class_="city-tile")				#取得連結子HTML
-all_link = [CityURL+a.get("href") for a in all_a ]			#得到一堆城市的網址
-#print(all_link)							#印出所有城市連結 (串列)
+sp = bs4.BeautifulSoup(result.content , "html.parser")								# 取的所有城市選單的HTML
+all_a = sp.find_all("a",class_="city-tile")											# 取得連結子HTML
+all_link = [CityURL+a.get("href") for a in all_a ]									# 得到一堆城市的網址
+# print(all_link)																	# 印出所有城市連結 (串列)
 
-for CityIndex in range(0,len(all_link)):				#印出所有城市連結 (單個)
-	#城市
-	#print(all_link[CityIndex])
-	InCityResult = requests.get(all_link[CityIndex])		#得到各地方城市的HTML
-	all_CityTitle = sp.find_all("span" ,class_="city-name")		#得到一堆城市的span程式
-	all_CityTitleName = [c.text for c in all_CityTitle]		#得到一堆城市的名字
-	#print str(all_CityTitleName[]).decode("unicode-escape")#Unicode反解
-	print(u'地區:' + all_CityTitleName[CityIndex])				#顯示台北市
-	for title in range(0,len(all_CityTitleName)):			#將所有城市的名字的數目下去做索引
-		#店家
-		InCitySp = bs4.BeautifulSoup(InCityResult.content , "html.parser")			#解析各程式HTML
-		InCityAllA = InCitySp.find_all("a",class_="hreview-aggregate url")			#查找所有a
-		InCityAllLink = [CityURL + b.get("href") for b in InCityAllA]				#將所有連結弄上來
-		#print(InCityAllLink)									#印出所有商店連結
-		for StoreIndex in range(0,len(InCityAllLink)):						#將所有商店連結的長度計次
-			StoreResult = requests.get(InCityAllLink[StoreIndex])				#將商店變成HTML
-			StoreSp = bs4.BeautifulSoup(StoreResult.content, "html.parser")			#解析
-			StoreName = StoreSp.find_all("h1",class_="fn")					#查找h1的類別為fn
-			#print(StoreName)								#印出店家HTML
-			print(u'店家:' + StoreName[0].text)						#印出店家
-			print('\t產品:')								#印出"產品"
-			MainProduct = StoreSp.find_all("h3" ,class_="dish-name fn p-name")		#查找品名稱
-			for MainProductIndex in range(len(MainProduct)):				#索引計次
-				#print('\t\t'+MainProduct[MainProductIndex].text)			#測試印出
-				print('\t\t'+(MainProduct[MainProductIndex].text.replace('\n','')))	#印出產品
-				Price = StoreSp.find_all("span" , class_="price p-price")		#尋找價格
-				print(u"\t\t價格" + Price[MainProductIndex].text)			#印出價格
+for CityIndex in range(0,len(all_link)):											# 印出所有城市連結 (單個)
+	# 城市
+	# print(all_link[CityIndex])
+	InCityResult = requests.get(all_link[CityIndex])								# 得到各地方城市的HTML
+	all_CityTitle = sp.find_all("span" ,class_="city-name")							# 得到一堆城市的span程式
+	all_CityTitleName = [c.text for c in all_CityTitle]								# 得到一堆城市的名字
+	# print str(all_CityTitleName[]).decode("unicode-escape")						# Unicode反解
+	print(u'地區:' + all_CityTitleName[CityIndex])									# 顯示台北市
+	for title in range(0,len(all_CityTitleName)):									# 將所有城市的名字的數目下去做索引
+		# 店家
+		InCitySp = bs4.BeautifulSoup(InCityResult.content , "html.parser")			# 解析各程式HTML
+		InCityAllA = InCitySp.find_all("a",class_="hreview-aggregate url")			# 查找所有a
+		InCityAllLink = [CityURL + b.get("href") for b in InCityAllA]				# 將所有連結弄上來
+		# print(InCityAllLink)														# 印出所有商店連結
+		for StoreIndex in range(0,len(InCityAllLink)):								# 將所有商店連結的長度計次
+			StoreResult = requests.get(InCityAllLink[StoreIndex])					# 將商店變成HTML
+			StoreSp = bs4.BeautifulSoup(StoreResult.content, "html.parser")			# 解析
+			StoreName = StoreSp.find_all("h1",class_="fn")							# 查找h1的類別為fn
+			# print(StoreName)														# 印出店家HTML
+			print(u'店家:' + StoreName[0].text)										# 印出店家
+			print('\t產品:')															# 印出"產品"
+			MainProduct = StoreSp.find_all("h3" ,class_="dish-name fn p-name")		# 查找品名稱
+			for MainProductIndex in range(len(MainProduct)):						# 索引計次
+				# print('\t\t'+MainProduct[MainProductIndex].text)					# 測試印出
+				print('\t\t'+(MainProduct[MainProductIndex].text.replace('\n','')))	# 印出產品
+				Price = StoreSp.find_all("span" , class_="price p-price")			# 尋找價格
+				print(u"\t\t價格" + Price[MainProductIndex].text)					# 印出價格
 
